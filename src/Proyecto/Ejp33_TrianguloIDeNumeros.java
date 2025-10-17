477f79ffc6f07f0f5ce7724be88ebcc1379a042f
jan1213540 <klebergutierrez56@gmail.com>
2025-10-17 09:14:21 -0500

    Correccion y agragado de imagen en ejercicio33

--- src/Proyecto/Ejp33_TrianguloIDeNumeros.java
+++ src/Proyecto/Ejp33_TrianguloIDeNumeros.java
@@ -1,17 +1,13 @@
 package Proyecto;
-
 import javax.swing.JOptionPane;
-
 /**
  *
  * @Jandry Gutierrez
  */
 public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
-
     public Ejp33_TrianguloIDeNumeros() {
         initComponents();
     }
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -23,6 +19,8 @@ public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtTriangulo = new javax.swing.JTextArea();
+        jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setLayout(new java.awt.BorderLayout());
 
@@ -46,7 +44,7 @@ public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
 
         txtBf.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jPanel1.add(txtBf);
-        txtBf.setBounds(150, 90, 146, 55);
+        txtBf.setBounds(240, 80, 146, 55);
 
         jButton1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jButton1.setText("GENERAR");
@@ -56,7 +54,7 @@ public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(339, 90, 130, 55);
+        jButton1.setBounds(410, 80, 130, 60);
 
         txtTriangulo.setColumns(20);
         txtTriangulo.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
@@ -64,7 +62,22 @@ public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
         jScrollPane2.setViewportView(txtTriangulo);
 
         jPanel1.add(jScrollPane2);
-        jScrollPane2.setBounds(110, 190, 430, 270);
+        jScrollPane2.setBounds(120, 220, 430, 270);
+
+        jButton2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jButton2.setText("LIMPIAR");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2);
+        jButton2.setBounds(410, 150, 130, 60);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Triangulo invertido.png"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(110, 0, 120, 220);
 
         add(jPanel1, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
@@ -94,11 +107,19 @@ public class Ejp33_TrianguloIDeNumeros extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+         txtBf.setText("");
+         txtTriangulo.setText("");
+    }//GEN-LAST:event_jButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextField txtBf;
